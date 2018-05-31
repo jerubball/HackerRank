9919a63dcef8d7087505b0d451e924035961cf76
Hasol Im <jerubball@gmail.com>
2018-05-31 19:55:08 -0400

    Create SimpleArraySum.java

---
+++ Algorithms/Warmup/SimpleArraySum.java
@@ -0,0 +1,35 @@
+package Algorithms.Warmup;
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+/**
+ * HackerRank Algorithms Warmup 2
+ * https://www.hackerrank.com/challenges/simple-array-sum/problem
+ * @author Hasol
+ */
+public class SimpleArraySum {
+    //Complete the simpleArraySum function below.
+    static int simpleArraySum(int[] ar) {
+        int s = 0;
+        for (int a: ar)
+            s += a;
+        return s;
+    }
+    static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        int arCount = Integer.parseInt(scanner.nextLine().trim());
+        int[] ar = new int[arCount];
+        String[] arItems = scanner.nextLine().split(" ");
+        for (int arItr=0; arItr<arCount; arItr++) {
+            int arItem = Integer.parseInt(arItems[arItr].trim());
+            ar[arItr] = arItem;
+        }
+        int result = simpleArraySum(ar);
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+        bufferedWriter.close();
+    }
+}
