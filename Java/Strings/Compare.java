4372b27cc8fd348bcf76e449ab17f2eff54f42ff
Hasol Im <jerubball@gmail.com>
2018-05-31 18:46:50 -0400

    Create Compare.java

---
+++ Java/Strings/Compare.java
@@ -0,0 +1,31 @@
+package Java.Strings;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+/**
+ * HackerRank Java Strings 3
+ * https://www.hackerrank.com/challenges/java-string-compare/problem
+ * @author Hasol
+ */
+public class Compare {
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        String[] str = new String[s.length()-k+1];
+        for (int i=0; i<str.length; i++)
+            str[i] = s.substring(i, i+k);
+        Arrays.sort(str);
+        smallest = str[0];
+        largest = str[str.length-1];
+        return smallest + "\n" + largest;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
