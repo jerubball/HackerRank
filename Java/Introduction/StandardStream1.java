5e1170bf1eec1653d6da27b30c6f9dcf41aab557
Hasol Im <jerubball@gmail.com>
2018-05-31 17:23:25 -0400

    Update StandardStream1.java

--- Java/Introduction/StandardStream1.java
+++ Java/Introduction/StandardStream1.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class StandardStream1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        for (int i = 0 ; i < 3; i++)
+        for (int i=0; i<3; i++)
             System.out.println(scan.nextInt());
     }
 }
