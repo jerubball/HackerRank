727573566d619b213f055d85ac8839b6812324b8
Hasol Im <jerubball@gmail.com>
2018-05-31 17:23:40 -0400

    Update Welcome.java

--- Java/Introduction/Welcome.java
+++ Java/Introduction/Welcome.java
@@ -5,8 +5,8 @@ package Java.Introduction;
  * @author Hasol
  */
 public class Welcome {
-    public static void main (String[] args) {
-        System.out.println ("Hello, World.");
-        System.out.println ("Hello, Java.");
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
     }
 }
