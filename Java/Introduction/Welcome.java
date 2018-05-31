9f582a9c4a5d4f53c3a1245bd3da625119be5a78
Hasol Im <jerubball@gmail.com>
2018-05-31 15:18:44 -0400

    Update Welcome.java

--- Java/Introduction/Welcome.java
+++ Java/Introduction/Welcome.java
@@ -1,3 +1,9 @@
+package Java.Introduction;
+/**
+ * HackerRank Java Introduction 1
+ * https://www.hackerrank.com/challenges/welcome-to-java/problem
+ * @author Hasol
+ */
 public class Welcome {
     public static void main(String[] args) {
         System.out.println ("Hello, World.");
