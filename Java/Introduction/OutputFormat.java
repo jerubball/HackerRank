4724815c243a9d6c484d96149eb1bbe439982750
Hasol Im <jerubball@gmail.com>
2018-05-31 17:24:41 -0400

    Update OutputFormat.java

--- Java/Introduction/OutputFormat.java
+++ Java/Introduction/OutputFormat.java
@@ -12,7 +12,7 @@ public class OutputFormat {
         for(int i=0; i<3; i++) {
             String s1=sc.next();
             int x=sc.nextInt();
-            System.out.printf ("%-15s%03d%n", s1, x);
+            System.out.printf("%-15s%03d%n", s1, x);
         }
         System.out.println("================================");
     }
