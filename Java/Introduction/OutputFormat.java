132dc54606830cede4578310fb7cf3e4cb35df1c
Hasol Im <jerubball@gmail.com>
2018-05-31 18:57:07 -0400

    Update OutputFormat.java

--- Java/Introduction/OutputFormat.java
+++ Java/Introduction/OutputFormat.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  */
 public class OutputFormat {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("================================");
         for(int i=0; i<3; i++) {
             String s1=sc.next();
