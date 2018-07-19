5b5ae1ecd3a1edd68bbef586f827754049d9a237
Hasol Im <jerubball@gmail.com>
2018-07-19 17:51:52 -0400

    Create BirdSing.java

---
+++ Java/ObjectOrientedProgramming/BirdSing.java
@@ -0,0 +1,32 @@
+package Java.ObjectOrientedProgramming;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+/**
+ * HackerRank Java ObjectOrientedProgramming 1
+ * https://www.hackerrank.com/challenges/java-inheritance-1/problem
+ * @author Hasol
+ */
+public class BirdSing {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
+}
+class Animal {
+    void walk() {
+        System.out.println("I am walking");
+    }
+}
+class Bird extends Animal {
+    void fly() {
+        System.out.println("I am flying");
+    }
+    void sing() {
+        System.out.println("I am singing");
+    }
+}
