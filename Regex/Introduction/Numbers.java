6fdcabd311a867bab3a94e3d70a1c82a73675f67
Hasol Im <jerubball@gmail.com>
2018-07-22 15:41:18 -0400

    Create Numbers.java

---
+++ Regex/Introduction/Numbers.java
@@ -0,0 +1,26 @@
+package Regex.Introduction;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+/**
+ * HackerRank Regex Introduction 3
+ * https://www.hackerrank.com/challenges/matching-digits-non-digit-character/problem
+ * @author Hasol
+ */
+public class Numbers {
+    public static void main(String[] args) {
+        RegexTest tester = new RegexTest();
+        tester.checker("\\d\\d\\D\\d\\d\\D\\d\\d\\d\\d");
+    }
+}
+class RegexTest {
+    public void checker(String RegexPattern) {
+        Scanner Input = new Scanner(System.in);
+        String TestString = Input.nextLine();
+        Pattern p = Pattern.compile(RegexPattern);
+        Matcher m = p.matcher(TestString);
+        System.out.println(m.find());
+    }
+}
