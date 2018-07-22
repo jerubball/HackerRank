dbfec800d8e0ae54cf5aded2746553ff30850c22
Hasol Im <jerubball@gmail.com>
2018-07-22 18:04:39 -0400

    Create ExactRepeat.java

---
+++ Regex/Repetitions/ExactRepeat.java
@@ -0,0 +1,26 @@
+package Regex.Repetitions;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+/**
+ * HackerRank Regex Repetitions 1
+ * https://www.hackerrank.com/challenges/matching-x-repetitions/problem
+ * @author Hasol
+ */
+public class ExactRepeat {
+    public static void main(String[] args) {
+        RegexTest tester = new RegexTest();
+        tester.checker("^[^\\W13579]{40}[\\s13579]{5}$");
+    }
+}
+class Regex_Test {
+    public void checker(String RegexPattern) {
+        Scanner Input = new Scanner(System.in);
+        String TestString = Input.nextLine();
+        Pattern p = Pattern.compile(RegexPattern);
+        Matcher m = p.matcher(TestString);
+        System.out.println(m.find());
+    }
+}
