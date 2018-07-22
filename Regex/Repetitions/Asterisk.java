11cb5997bf9c5a28818844564d6f2e8b9dbd340c
Hasol Im <jerubball@gmail.com>
2018-07-22 18:27:22 -0400

    Create Asterisk.java

---
+++ Regex/Repetitions/Asterisk.java
@@ -0,0 +1,26 @@
+package Regex.Repetitions;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+/**
+ * HackerRank Regex Repetitions 3
+ * https://www.hackerrank.com/challenges/matching-zero-or-more-repetitions/problem
+ * @author Hasol
+ */
+public class Asterisk {
+    public static void main(String[] args) {
+        RegexTest tester = new RegexTest();
+        tester.checker("^\\d{2,}[a-z]*[A-Z]*$");
+    }
+}
+class RegexTest {
+    public void checker(String RegexPattern) {
+        Scanner Input = new Scanner(System.in);
+        String TestString = Input.nextLine();
+        Pattern p = Pattern.compile(RegexPattern);
+        Matcher m = p.matcher(TestString);
+        System.out.println(m.find());
+    }
+}
