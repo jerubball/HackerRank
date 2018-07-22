5515866a3694977df83a91d772e06c553d14896e
Hasol Im <jerubball@gmail.com>
2018-07-22 15:55:40 -0400

    Create readme.java

---
+++ Regex/CharacterClass/readme.java
@@ -0,0 +1,11 @@
+## Table of Contents for Regex/[Character Class](https://www.hackerrank.com/domains/regex?filters%5Bsubdomains%5D%5B%5D=re-character-class)
+
+| #  | File Name                                            | Title                           | Difficulty | Max Score |
+| -- | ---------------------------------------------------- | ------------------------------- | ---------- | --------- |
+| 1  | [.java](.java)                                       | [Matching Specific Characters]  | Easy       | 10        |
+| 2  | [.java](.java)                                       | [Excluding Specific Characters] | Easy       | 10        |
+| 3  | [.java](.java)                                       | [Matching Character Ranges]     | Easy       | 10        |
+
+[Matching Specific Characters]: 
+[Excluding Specific Characters]: 
+[Matching Character Ranges]: 
