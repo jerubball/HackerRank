0bd2b2890382b152a7bdf7b1c0330efd0a09db82
Hasol Im <jerubball@gmail.com>
2018-07-24 00:24:08 -0400

    Create PositiveLookbehind.java

---
+++ Regex/Assertions/PositiveLookbehind.java
@@ -0,0 +1,29 @@
+package Regex.Assertions;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+/**
+ * HackerRank Regex Assertions 3
+ * https://www.hackerrank.com/challenges/positive-lookbehind/problem
+ * @author Hasol
+ */
+public class PositiveLookbehind {
+    public static void main(String[] args) {
+        Regex_Test tester = new Regex_Test();
+        tester.checker("(?<=[13579])\\d");
+    }
+}
+class RegexTest {
+    public void checker(String RegexPattern){
+        Scanner Input = new Scanner(System.in);
+        String TestString = Input.nextLine();
+        Pattern p = Pattern.compile(RegexPattern);
+        Matcher m = p.matcher(TestString);
+        int count = 0;
+        while (m.find())
+            count += 1;
+        System.out.format("Number of matches : %d", count);
+    }
+}
