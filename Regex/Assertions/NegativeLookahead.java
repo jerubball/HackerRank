4026fc5d6064781ae7013ffb6dff8c0bbf2af104
Hasol Im <jerubball@gmail.com>
2018-07-24 00:26:37 -0400

    Update NegativeLookahead.java

--- Regex/Assertions/NegativeLookahead.java
+++ Regex/Assertions/NegativeLookahead.java
@@ -11,7 +11,7 @@ import java.util.regex.*;
  */
 public class NegativeLookahead {
     public static void main(String[] args) {
-        Regex_Test tester = new Regex_Test();
+        RegexTest tester = new RegexTest();
         tester.checker("(.)(?!\\1)");
     }
 }
