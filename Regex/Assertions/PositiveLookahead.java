66b17a506dddbe25f0bcca7a3da45894db320395
Hasol Im <jerubball@gmail.com>
2018-07-24 00:26:28 -0400

    Update PositiveLookahead.java

--- Regex/Assertions/PositiveLookahead.java
+++ Regex/Assertions/PositiveLookahead.java
@@ -11,7 +11,7 @@ import java.util.regex.*;
  */
 public class PositiveLookahead {
     public static void main(String[] args) {
-        Regex_Test tester = new Regex_Test();
+        RegexTest tester = new RegexTest();
         tester.checker("o(?=oo)");
     }
 }
