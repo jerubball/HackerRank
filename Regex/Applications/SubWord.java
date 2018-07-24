a82b3e24ff0143fb25f117347c4ceec02b85c2c5
Hasol Im <jerubball@gmail.com>
2018-07-24 14:32:23 -0400

    Create SubWord.java

---
+++ Regex/Applications/SubWord.java
@@ -0,0 +1,31 @@
+package Regex.Applications;
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+/**
+ * HackerRank Regex Applications 3
+ * https://www.hackerrank.com/challenges/find-substring/problem
+ * @author Hasol
+ */
+public class SubWord {
+    static int numMatches(CharSequence seq, String reg) {
+        int n = 0;
+        Matcher m = Pattern.compile(reg).matcher(seq);
+        while (m.find())
+            n++;
+        return n;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        StringBuilder sb = new StringBuilder(100*n);
+        HashSet<String> set = new HashSet<>();
+        for (int i=0; i<n; i++) {
+            sb.append(scan.nextLine());
+            sb.append("\n");
+        }
+        int q = Integer.parseInt(scan.nextLine());
+        for (int i=0; i<q; i++)
+            System.out.println(numMatches(sb, "\\w" + scan.nextLine() + "\\w"));
+    }
+}
