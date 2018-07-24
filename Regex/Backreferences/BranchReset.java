e0427385e53fed2886c561b3e03339b3d76eeefa
Hasol Im <jerubball@gmail.com>
2018-07-23 23:51:47 -0400

    Create BranchReset.java

---
+++ Regex/Backreferences/BranchReset.java
@@ -0,0 +1,27 @@
+package Regex.Backreferences;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+/**
+ * HackerRank Regex Backreferences 3
+ * https://www.regular-expressions.info/branchreset.html
+ * @author Hasol
+ */
+public class BranchReset {    
+    public static void main(String[] args) {
+        RegexTest tester = new RegexTest();
+        // Branch Reset not supproted in Java. see BranchReset.php
+        tester.checker("^\\d{2}([-.:]|---)(\\d{2}\\1){2}\\d{2}$");
+    }
+}
+class RegexTest {
+    public void checker(String RegexPattern) {
+        Scanner Input = new Scanner(System.in);
+        String TestString = Input.nextLine();
+        Pattern p = Pattern.compile(RegexPattern);
+        Matcher m = p.matcher(TestString);
+        System.out.println(m.find());
+    }
+}
