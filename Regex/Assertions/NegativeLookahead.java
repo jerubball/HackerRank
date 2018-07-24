ce5e6ff8c80cd297a59548a2a10b363b6ad65db0
Hasol Im <jerubball@gmail.com>
2018-07-24 00:20:52 -0400

    Create NegativeLookahead.java

---
+++ Regex/Assertions/NegativeLookahead.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+package Regex.Assertions;
+/**
+ * HackerRank Regex Assertions 2
+ * https://www.hackerrank.com/challenges/negative-lookahead/problem
+ * @author Hasol
+ */
+public class NegativeLookahead {
+    public static void main(String[] args) {
+        Regex_Test tester = new Regex_Test();
+        tester.checker("(.)(?!\\1)");
+    }
+}
+class RegexTest {
+    public void checker(String RegexPattern){
+        Scanner Input = new Scanner(System.in);
+        String TestString = Input.nextLine();
+        Pattern p = Pattern.compile(RegexPattern);
+        Matcher m = p.matcher(TestString);
+        int count = 0;
+        while (m.find())
+            count += 1;
+        System.out.format("Number of matches : %d", count);
+    }
+}
