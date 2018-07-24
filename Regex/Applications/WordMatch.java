ea7ba3092da398f403f3d089a10cd1a55669d511
Hasol Im <jerubball@gmail.com>
2018-07-24 14:47:05 -0400

    Create WordMatch.java

---
+++ Regex/Applications/WordMatch.java
@@ -0,0 +1,31 @@
+package Regex.Applications;
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+/**
+ * HackerRank Regex Applications 6
+ * https://www.hackerrank.com/challenges/find-a-word/problem
+ * @author Hasol
+ */
+public class WordMatch {
+    static int numMatches(CharSequence seq, String reg) {
+        int n = 0;
+        Matcher m = Pattern.compile(reg).matcher(seq);
+        while (m.find())
+            n++;
+        return n;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        StringBuilder sb = new StringBuilder(100*n);
+        HashSet<String> set = new HashSet<>();
+        for (int i=0; i<n; i++) {
+            sb.append(scan.nextLine());
+            sb.append("\n");
+        }
+        int t = Integer.parseInt(scan.nextLine());
+        for (int i=0; i<t; i++)
+            System.out.println(numMatches(sb, "\\b" + scan.nextLine() + "\\b"));
+    }
+}
