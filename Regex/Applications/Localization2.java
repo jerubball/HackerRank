25d3e16598e561721af3e924c7b40a136fef567d
Hasol Im <jerubball@gmail.com>
2018-08-03 18:42:25 -0400

    Create Localization2.java

---
+++ Regex/Applications/Localization2.java
@@ -0,0 +1,33 @@
+package Regex.Applications;
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+/**
+ * HackerRank Regex Applications 22
+ * https://www.hackerrank.com/challenges/uk-and-us-2/problem
+ * @author Hasol
+ */
+public class Localization2 {
+    static int numMatches(CharSequence seq, String reg) {
+        int n = 0;
+        Matcher m = Pattern.compile(reg).matcher(seq);
+        while (m.find())
+            n++;
+        return n;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<n; i++) {
+            sb.append(scan.nextLine());
+            sb.append('\n');
+        }
+        int q = Integer.parseInt(scan.nextLine());
+        for (int i=0; i<q; i++) {
+            String w = scan.nextLine();
+            w = Pattern.compile("our").matcher(w).replaceFirst("ou?r");
+            System.out.println(numMatches(sb, "\\b" + w + "\\b"));
+        }
+    }
+}
