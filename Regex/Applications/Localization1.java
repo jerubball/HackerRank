ab69e4691e64133f0bc03b400ea19c2f85cb5d32
Hasol Im <jerubball@gmail.com>
2018-08-03 18:29:01 -0400

    Create Localization1.java

---
+++ Regex/Applications/Localization1.java
@@ -0,0 +1,32 @@
+package Regex.Applications;
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+/**
+ * HackerRank Regex Applications 21
+ * https://www.hackerrank.com/challenges/uk-and-us/problem
+ * @author Hasol
+ */
+public class Localization1 {
+    static int numMatches(CharSequence seq, String reg) {
+        int n = 0;
+        Matcher m = Pattern.compile(reg).matcher(seq);
+        while (m.find())
+            n++;
+        return n;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<n; i++) {
+            sb.append(scan.nextLine());
+            sb.append('\n');
+        }
+        int q = Integer.parseInt(scan.nextLine());
+        for (int i=0; i<q; i++) {
+            String w = scan.nextLine();
+            System.out.println(numMatches(sb, w.substring(0, w.length()-2) + "[sz]e"));
+        }
+    }
+}
