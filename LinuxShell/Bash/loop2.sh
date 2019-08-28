b2f819628fa62dac073710e76b9cb8cf7855bfe6
Hasol Im <jerubball@gmail.com>
2019-08-28 16:54:14 -0400

    Create loop2.sh

---
+++ LinuxShell/Bash/loop2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# HackerRank LinuxShell Bash 4
+# https://www.hackerrank.com/challenges/bash-tutorials---looping-with-numbers/problem
+# Author: Hasol
+
+for (( i=1; i<=50; i++ ))
+do
+    echo $i
+done
