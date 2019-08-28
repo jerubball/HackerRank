2ecd74b8860838b8e228d2c007722be89bef903c
Hasol Im <jerubball@gmail.com>
2019-08-28 16:45:00 -0400

    Create loop.sh

---
+++ LinuxShell/Bash/loop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# HackerRank LinuxShell Bash 2
+# https://www.hackerrank.com/challenges/bash-tutorials---looping-and-skipping/problem
+# Author: Hasol
+
+for i in {1..99..2}
+do
+    echo $i
+done
