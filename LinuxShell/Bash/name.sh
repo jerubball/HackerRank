ed09fadf48b1f532c41cbc68df55b70cbb11cedd
Hasol Im <jerubball@gmail.com>
2019-08-28 16:50:19 -0400

    Create name.sh

---
+++ LinuxShell/Bash/name.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# HackerRank LinuxShell Bash 3
+# https://www.hackerrank.com/challenges/bash-tutorials---a-personalized-echo/problem
+# Author: Hasol
+
+read name
+echo "Welcome $name"
+#echo "Welcome $1"
