ab2af8b7a1f3fc003df1dd4438afcdf006884d5d
Hasol Im <jerubball@gmail.com>
2019-08-28 17:31:26 -0400

    Create evaluate.sh

---
+++ LinuxShell/Bash/evaluate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# HackerRank LinuxShell Bash 9
+# https://www.hackerrank.com/challenges/bash-tutorials---arithmetic-operations/problem
+# Author: Hasol
+
+read e
+
+printf "%.3f" $(bc -l <<< $e)
+#bc <<< "scale=3; $e"
