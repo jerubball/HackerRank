ca9b5e2d8eb5a2b97c3d2ecde4746325a01298eb
Hasol Im <jerubball@gmail.com>
2019-08-29 14:51:19 -0400

    Create headtail.sh

---
+++ LinuxShell/TextProcessing/headtail.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# HackerRank LinuxShell TextProcessing 12
+# https://www.hackerrank.com/challenges/text-processing-in-linux---the-middle-of-a-text-file/problem
+# Author: Hasol
+
+head -22 | tail -11
