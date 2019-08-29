32bd47161e1730d304633950d1e4edb1ce00d4a1
Hasol Im <jerubball@gmail.com>
2019-08-29 16:38:05 -0400

    Create uniq2.sh

---
+++ LinuxShell/TextProcessing/uniq2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# HackerRank LinuxShell TextProcessing 26
+# https://www.hackerrank.com/challenges/text-processing-in-linux-the-uniq-command-2/problem
+# Author: Hasol
+
+#uniq -c
+uniq -c | cut -c7-
