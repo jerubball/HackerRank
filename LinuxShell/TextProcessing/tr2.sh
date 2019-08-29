427cf9fd86354e1afac06296babd8b491582d5e1
Hasol Im <jerubball@gmail.com>
2019-08-29 15:30:30 -0400

    Create tr2.sh

---
+++ LinuxShell/TextProcessing/tr2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# HackerRank LinuxShell TextProcessing 16
+# https://www.hackerrank.com/challenges/text-processing-tr-2/problem
+# Author: Hasol
+
+tr -d [a-z]
+
+#while read i
+#do
+#    echo ${i//[a-z]/}
+#done
