ce6d703352fbe9d498651339160cf8834bcc7298
Hasol Im <jerubball@gmail.com>
2019-08-29 22:00:45 -0400

    Create grep2.sh

---
+++ LinuxShell/GrepSedAwk/grep2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# HackerRank LinuxShell GrepSedAwk 4
+# https://www.hackerrank.com/challenges/text-processing-in-linux-the-grep-command-2/problem
+# Author: Hasol
+
+grep -E -i '.*\bthe\b.*'
