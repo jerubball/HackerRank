012f21802c6590ae6e3742fd9db96d6463928c8f
Hasol Im <jerubball@gmail.com>
2019-08-29 22:07:56 -0400

    Create grep5.sh

---
+++ LinuxShell/GrepSedAwk/grep5.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# HackerRank LinuxShell GrepSedAwk 7
+# https://www.hackerrank.com/challenges/text-processing-in-linux-the-grep-command-5/problem
+# Author: Hasol
+
+grep -E '.*([0-9])[ ]?\1.*'
