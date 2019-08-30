7e88621216a7c99eb4528979e25a61ca93935963
Hasol Im <jerubball@gmail.com>
2019-08-29 22:23:01 -0400

    Create sed2.sh

---
+++ LinuxShell/GrepSedAwk/sed2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# HackerRank LinuxShell GrepSedAwk 9
+# https://www.hackerrank.com/challenges/text-processing-in-linux-the-sed-command-2/problem
+# Author: Hasol
+
+sed -E -e 's/\bthy\b/your/gi'
