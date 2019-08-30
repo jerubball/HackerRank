65223d06470d85e7b56550ec282b4ce52c2e6140
Hasol Im <jerubball@gmail.com>
2019-08-29 22:05:55 -0400

    Create grep4.sh

---
+++ LinuxShell/GrepSedAwk/grep4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# HackerRank LinuxShell GrepSedAwk 6
+# https://www.hackerrank.com/challenges/text-processing-in-linux-the-grep-command-4/problem
+# Author: Hasol
+
+grep -E -i '.*\b(the|that|then|those)\b.*'
