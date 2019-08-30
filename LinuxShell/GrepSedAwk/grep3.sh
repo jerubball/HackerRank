9eb48809303a21ce83eee5f3b29eff0fa167153f
Hasol Im <jerubball@gmail.com>
2019-08-29 22:04:35 -0400

    Create grep3.sh

---
+++ LinuxShell/GrepSedAwk/grep3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# HackerRank LinuxShell GrepSedAwk 5
+# https://www.hackerrank.com/challenges/text-processing-in-linux-the-grep-command-3/problem
+# Author: Hasol
+
+grep -E -i -v '.*\bthat\b.*'
