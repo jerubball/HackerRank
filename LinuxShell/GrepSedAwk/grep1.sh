fab8528b9261bab51cfb42c90791f9b8773c025f
Hasol Im <jerubball@gmail.com>
2019-08-29 21:59:20 -0400

    Create grep1.sh

---
+++ LinuxShell/GrepSedAwk/grep1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# HackerRank LinuxShell GrepSedAwk 3
+# https://www.hackerrank.com/challenges/text-processing-in-linux-the-grep-command-1/problem
+# Author: Hasol
+
+grep -E '.+ the .+'
