5b7127ec3e9a79d4f53a7de506be8f435c5db7b3
Hasol Im <jerubball@gmail.com>
2019-08-29 22:18:51 -0400

    Create sed1.sh

---
+++ LinuxShell/GrepSedAwk/sed1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# HackerRank LinuxShell GrepSedAwk 8
+# https://www.hackerrank.com/challenges/text-processing-in-linux-the-sed-command-1/problem
+# Author: Hasol
+
+sed -E -e 's/\bthe\b/this/'
