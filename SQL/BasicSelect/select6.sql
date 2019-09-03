4eb74bcc5a1d076a4e06be47c728ed9cce0387bc
Hasol Im <jerubball@gmail.com>
2019-09-03 18:36:56 -0400

    Create select6.sql

---
+++ SQL/BasicSelect/select6.sql
@@ -0,0 +1,7 @@
+-- MySQL
+
+-- HackerRank SQL BasicSelect 6
+-- https://www.hackerrank.com/challenges/japanese-cities-name/problem
+-- Author: Hasol
+
+select NAME from CITY where COUNTRYCODE = 'JPN';
