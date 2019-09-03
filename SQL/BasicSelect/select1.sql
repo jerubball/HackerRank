e3d8459bfc8f46c46d46d2e84a1dfd648a0ce803
Hasol Im <jerubball@gmail.com>
2019-09-03 18:11:18 -0400

    Update select1.sql

--- SQL/BasicSelect/select1.sql
+++ SQL/BasicSelect/select1.sql
@@ -4,4 +4,4 @@
 -- https://www.hackerrank.com/challenges/revising-the-select-query/problem
 -- Author: Hasol
 
-select * from city where countrycode = 'USA' and population > 100000;
+select * from CITY where COUNTRYCODE = 'USA' and POPULATION > 100000;
