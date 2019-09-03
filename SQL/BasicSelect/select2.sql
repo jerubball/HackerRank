cc1466ad78517bc808e0980427e219e488170f37
Hasol Im <jerubball@gmail.com>
2019-09-03 18:11:53 -0400

    Create select2.sql

---
+++ SQL/BasicSelect/select2.sql
@@ -0,0 +1,7 @@
+-- MySQL
+
+-- HackerRank SQL BasicSelect 2
+-- https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+-- Author: Hasol
+
+select NAME from CITY where COUNTRYCODE = 'USA' and POPULATION > 120000;
