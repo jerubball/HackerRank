1101b33d26b4131521cbd83d5c52808a7c7bd298
Hasol Im <jerubball@gmail.com>
2019-09-03 18:18:03 -0400

    Update DB_city.sql

--- SQL/BasicSelect/DB_city.sql
+++ SQL/BasicSelect/DB_city.sql
@@ -10,3 +10,16 @@ create table CITY (
   DISTRICT varchar(20),
   POPULATION int
 );
+
+insert into CITY values
+  (6,    "Rotterdam",     "NLD", "Zuid-Holland",  593321),
+  (3878, "Scottsdale",    "USA", "Arizona",       202705),
+  (3965, "Corona",        "USA", "California",    124966),
+  (3973, "Concord",       "USA", "California",    121780),
+  (3977, "Cedar Rapids",  "USA", "Iowa",          120758),
+  (3982, "Coral Springs", "USA", "Florida",       117549),
+  (4054, "Fairfield",     "USA", "California",    92256),
+  (4058, "Boulder",       "USA", "Colorado",      91238),
+  (4061, "Fall River",    "USA", "Massachusetts", 90555);
+
+
