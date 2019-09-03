2a3acf42ee9010f6393bba4971cb547715b30434
Hasol Im <jerubball@gmail.com>
2019-09-03 18:36:12 -0400

    Create select5.sql

---
+++ SQL/BasicSelect/select5.sql
@@ -0,0 +1,7 @@
+-- MySQL
+
+-- HackerRank SQL BasicSelect 5
+-- https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+-- Author: Hasol
+
+select * from CITY where COUNTRYCODE = 'JPN';
