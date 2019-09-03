48f8fc7ff5a1d8de973e5789c8689f862eab6603
Hasol Im <jerubball@gmail.com>
2019-09-03 18:10:20 -0400

    Create DB_city.sql

---
+++ SQL/BasicSelect/DB_city.sql
@@ -0,0 +1,12 @@
+-- MySQL
+
+create database BasicSelect;
+use BasicSelect;
+
+create table CITY (
+  ID int,
+  NAME varchar(17),
+  COUNTRYCODE varchar(3),
+  DISTRICT varchar(20),
+  POPULATION int
+);
