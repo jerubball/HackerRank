00a2a8199ea882b43cefa2a27cb609a930e3c2df
Hasol Im <jerubball@gmail.com>
2019-09-03 20:24:15 -0400

    Create employee2.sql

---
+++ SQL/BasicSelect/employee2.sql
@@ -0,0 +1,7 @@
+-- MySQL
+
+-- HackerRank SQL BasicSelect 20
+-- https://www.hackerrank.com/challenges/salary-of-employees/problem
+-- Author: Hasol
+
+select name from Employee where salary > 2000 and months < 10 order by employee_id;
