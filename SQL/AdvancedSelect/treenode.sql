a1ccc3f22c054aa2202a0badcf8ca1198e19abdc
Hasol Im <jerubball@gmail.com>
2019-09-04 19:49:49 -0400

    Update treenode.sql

--- SQL/AdvancedSelect/treenode.sql
+++ SQL/AdvancedSelect/treenode.sql
@@ -8,3 +8,5 @@ select N, case when P is null then 'Root'
                when (select count(P) from BST as TREE where TREE.P = BST.N) > 0 then 'Inner'
                else 'Leaf'
           end from BST order by N;
+
+-- select N, if(P is null,'Root',if((select count(*) from BST where P=B.N)>0,'Inner','Leaf')) from BST as B order by N;
