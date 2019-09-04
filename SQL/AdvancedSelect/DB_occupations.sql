0c6752199e4bd372bf74e0f1bde7dc524233274e
Hasol Im <jerubball@gmail.com>
2019-09-03 22:01:00 -0400

    Create DB_occupations.sql

---
+++ SQL/AdvancedSelect/DB_occupations.sql
@@ -0,0 +1,32 @@
+-- MySQL
+
+create database if not exists AdvancedSelect;
+use AdvancedSelect;
+
+drop table if not exists OCCUPATIONS;
+create table OCCUPATIONS (
+  Name       varchar(30),
+  Occupation varchar(30)
+);
+
+insert into OCCUPATIONS values
+  ('Ashley',    'Professor'),
+  ('Samantha',  'Actor'),
+  ('Julia',     'Doctor'),
+  ('Britney',   'Professor'),
+  ('Maria',     'Professor'),
+  ('Meera',     'Professor'),
+  ('Priya',     'Doctor'),
+  ('Priyanka',  'Professor'),
+  ('Jennifer',  'Actor'),
+  ('Ketty',     'Actor'),
+  ('Belvet',    'Professor'),
+  ('Naomi',     'Professor'),
+  ('Jane',      'Singer'),
+  ('Jenny',     'Singer'),
+  ('Kristeen',  'Singer'),
+  ('Christeen', 'Singer'),
+  ('Eve',       'Actor'),
+  ('Aamina',    'Doctor');
+
+
