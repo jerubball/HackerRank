b1cea4fedc85f12f030e4d672f6308caa25e189c
Hasol Im <jerubball@gmail.com>
2019-09-03 20:10:20 -0400

    Update DB_students.sql

--- SQL/BasicSelect/DB_students.sql
+++ SQL/BasicSelect/DB_students.sql
@@ -9,3 +9,32 @@ create table STUDENTS (
   Name  varchar(30),
   Marks int
 );
+
+insert into STUDENTS values
+  (11, 'Britney',   95),
+  (12, 'Dyana',     55),
+  (13, 'Jenny',     66),
+  (14, 'Christene', 88),
+  (15, 'Meera',     24),
+  (16, 'Devil',     76),
+  (16, 'Priya',     76),
+  (17, 'Priyanka',  77),
+  (17, 'Evil',      79),
+  (18, 'Paige',     74),
+  (19, 'Samantha',  87),
+  (19, 'Jane',      64),
+  (21, 'Julia',     96),
+  (21, 'Belvet',    78),
+  (31, 'Scarlet',   80),
+  (32, 'Kristeen',  100),
+  (34, 'Fanny',     75),
+  (38, 'Danny',     75),
+  (41, 'Salma',     81),
+  (51, 'Amanda',    34),
+  (61, 'Heraldo',   94),
+  (71, 'Stuart',    99),
+  (76, 'Amina',     89),
+  (81, 'Aamina',    77),
+  (91, 'Vivek',     84);
+
+
