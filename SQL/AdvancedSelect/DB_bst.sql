66d0e2871c8c053114f323579652dbffb25eb6e5
Hasol Im <jerubball@gmail.com>
2019-09-03 22:05:54 -0400

    Create DB_bst.sql

---
+++ SQL/AdvancedSelect/DB_bst.sql
@@ -0,0 +1,29 @@
+-- MySQL
+
+create database if not exists AdvancedSelect;
+use AdvancedSelect;
+
+drop table if exists BST;
+create table BST (
+  N int,
+  P int
+);
+
+insert into BST values
+  (1,  2),
+  (3,  2),
+  (5,  6),
+  (7,  6),
+  (2,  4),
+  (6,  4),
+  (4,  15),
+  (8,  9),
+  (10, 9),
+  (12, 13),
+  (14, 13),
+  (9,  11),
+  (13, 11),
+  (11, 15),
+  (15, NULL);
+
+
