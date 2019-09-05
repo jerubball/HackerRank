8c745929fb514089b209f603127b2a01d49be6b4
Hasol Im <jerubball@gmail.com>
2019-09-05 15:36:09 -0400

    Create city9.sql

---
+++ SQL/Aggregation/city9.sql
@@ -0,0 +1,7 @@
+-- MySQL
+
+-- HackerRank SQL Aggregation 3
+-- https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+-- Author: Hasol
+
+select avg(POPULATION) from CITY where DISTRICT = 'California' and COUNTRYCODE = 'USA';
