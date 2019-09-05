769fb2ba87cdcee505a09bdccd3ee425daadf5be
Hasol Im <jerubball@gmail.com>
2019-09-05 15:33:20 -0400

    Create city7.sql

---
+++ SQL/Aggregation/city7.sql
@@ -0,0 +1,7 @@
+-- MySQL
+
+-- HackerRank SQL Aggregation 1
+-- https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+-- Author: Hasol
+
+select count(*) from CITY where POPULATION > 100000;
