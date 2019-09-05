62924967c22f7788cd5134c0fb7780f62aa4c1a9
Hasol Im <jerubball@gmail.com>
2019-09-05 16:36:25 -0400

    Create station17.sql

---
+++ SQL/Aggregation/station17.sql
@@ -0,0 +1,7 @@
+-- MySQL
+
+-- HackerRank SQL Aggregation 14
+-- https://www.hackerrank.com/challenges/weather-observation-station-17/problem
+-- Author: Hasol
+
+select round(LONG_W,4) from STATION where LAT_N = (select min(LAT_N) from STATION where LAT_N > 38.7780);
