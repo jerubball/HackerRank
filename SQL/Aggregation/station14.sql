1a8d7234a4a9d173a0f2a16a3261346a7b3c5cd9
Hasol Im <jerubball@gmail.com>
2019-09-05 16:31:22 -0400

    Create station14.sql

---
+++ SQL/Aggregation/station14.sql
@@ -0,0 +1,7 @@
+-- MySQL
+
+-- HackerRank SQL Aggregation 11
+-- https://www.hackerrank.com/challenges/weather-observation-station-14/problem
+-- Author: Hasol
+
+select round(max(LAT_N),4) from STATION where LAT_N < 137.2345;
