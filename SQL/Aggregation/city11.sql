673fa5d6fa8045cb333a67e70bd82605fdd1acdd
Hasol Im <jerubball@gmail.com>
2019-09-05 15:38:32 -0400

    Create city11.sql

---
+++ SQL/Aggregation/city11.sql
@@ -0,0 +1,7 @@
+-- MySQL
+
+-- HackerRank SQL Aggregation 5
+-- https://www.hackerrank.com/challenges/japan-population/problem
+-- Author: Hasol
+
+select sum(POPULATION) from CITY where COUNTRYCODE = 'JPN';
