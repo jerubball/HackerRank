940a3cb9a06ba284a803740df9cbf8a2511847f2
Hasol Im <jerubball@gmail.com>
2019-09-05 15:39:37 -0400

    Create city12.sql

---
+++ SQL/Aggregation/city12.sql
@@ -0,0 +1,7 @@
+-- MySQL
+
+-- HackerRank SQL Aggregation 6
+-- https://www.hackerrank.com/challenges/population-density-difference/problem
+-- Author: Hasol
+
+select max(POPULATION) - min(POPULATION) from CITY;
