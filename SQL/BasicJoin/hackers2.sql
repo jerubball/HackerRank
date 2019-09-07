9e0d90b41f698befb47c57cf39dab3e51efeb995
Hasol Im <jerubball@gmail.com>
2019-09-07 14:35:11 -0400

    Create hackers2.sql

---
+++ SQL/BasicJoin/hackers2.sql
@@ -0,0 +1,22 @@
+-- MySQL
+
+-- HackerRank SQL BasicJoin 7
+-- https://www.hackerrank.com/challenges/challenges/problem
+-- Author: Hasol
+
+-- select Hackers.hacker_id, Hackers.name, count(challenge_id) from Hackers inner join Challenges using (hacker_id) group by Hackers.hacker_id, Hackers.name order by count(challenge_id) desc;
+
+select Hackers.hacker_id, Hackers.name, Result4.challenges_created
+    from Hackers inner join (select Hackers.hacker_id as hacker_id, count(challenge_id) as challenges_created
+                                from Hackers inner join Challenges using (hacker_id)
+                                group by Hackers.hacker_id) as Result4 using (hacker_id)
+    where Result4.challenges_created not in
+        (select challenges_created
+            from (select challenges_created, count(challenges_created) as challenge_count
+                    from (select Hackers.hacker_id as hacker_id, count(challenge_id) as challenges_created
+                            from Hackers inner join Challenges using (hacker_id) group by Hackers.hacker_id) as Result1
+                    where challenges_created < (select max(challenges_created) from (select Hackers.hacker_id as hacker_id, count(challenge_id) as challenges_created
+                            from Hackers inner join Challenges using (hacker_id) group by Hackers.hacker_id) as Result2)
+                    group by challenges_created) as Result3
+            where challenge_count > 1)
+    order by Result4.challenges_created desc, Hackers.hacker_id;
