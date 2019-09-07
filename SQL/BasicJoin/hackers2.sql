1bfec57a65810fc6a3819314a1bd2f41528f0b5e
Hasol Im <jerubball@gmail.com>
2019-09-07 19:09:43 -0400

    Update hackers2.sql

--- SQL/BasicJoin/hackers2.sql
+++ SQL/BasicJoin/hackers2.sql
@@ -20,3 +20,17 @@ select Hackers.hacker_id, Hackers.name, Result4.challenges_created
                     group by challenges_created) as Result3
             where challenge_count > 1)
     order by Result4.challenges_created desc, Hackers.hacker_id;
+
+/*
+select Hackers.hacker_id, Hackers.name, Result1.challenges_created
+    from Hackers inner join (select Hackers.hacker_id as hacker_id, count(challenge_id) as challenges_created
+                                from Hackers inner join Challenges using (hacker_id)
+                                group by Hackers.hacker_id) as Result1 using (hacker_id)
+    where Result1.challenges_created not in
+        (select challenges_created
+            from (select Hackers.hacker_id as hacker_id, count(challenge_id) as challenges_created
+                    from Hackers inner join Challenges using (hacker_id) group by Hackers.hacker_id) as Result2
+            group by challenges_created having count(challenges_created) > 1
+            order by challenges_created desc limit 214783647 offset 1)
+    order by Result1.challenges_created desc, Hackers.hacker_id;
+*/
