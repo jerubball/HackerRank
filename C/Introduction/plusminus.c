512d4149c034f47a65455b9c7bf1c0d3c1abbdef
Hasol Im <jerubball@gmail.com>
2019-09-08 08:47:22 -0400

    Create plusminus.c

---
+++ C/Introduction/plusminus.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+/*
+ * HackerRank C Introduction 3
+ * https://www.hackerrank.com/challenges/sum-numbers-c/problem
+ * Author: Hasol
+ */
+int main()
+{
+	int a, b;
+    float c, d;
+    scanf ("%d %d%*[\r\n]", &a, &b);
+    scanf ("%f %f%*[\r\n]", &c, &d);
+    printf ("%d %d\n", a+b, a-b);
+    printf ("%.1f %.1f", c+d, c-d);
+    return 0;
+}
+
