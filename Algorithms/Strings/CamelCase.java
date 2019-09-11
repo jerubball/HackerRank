ea69aae08abdd92927959499b2af92070c8e4088
Hasol Im <jerubball@gmail.com>
2019-09-11 16:40:01 -0400

    Create CamelCase.java

---
+++ Algorithms/Strings/CamelCase.java
@@ -0,0 +1,33 @@
+package Algorithms.Strings;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+/**
+ * HackerRank Algorithms Strings 2
+ * https://www.hackerrank.com/challenges/camelcase/problem
+ * @author Hasol
+ */
+public class Solution {
+    // Complete the camelcase function below.
+    static int camelcase(String s) {
+        int count = 1;
+        for (char c: s.toCharArray())
+            if (c >= 'A' && c <= 'Z')
+                count++;
+        return count;
+    }
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        String s = scanner.nextLine();
+        int result = camelcase(s);
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+        bufferedWriter.close();
+        scanner.close();
+    }
+}
